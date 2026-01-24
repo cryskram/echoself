b1b9c74e8949d1d55843d2950ef312cb871541fe
cryskram <vageeshgn2005@gmail.com>
2026-01-24 19:16:24 +0530

    update: url visibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,27 @@ const font = IBM_Plex_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "EchoSelf",
   description: "See yourself. Hear yourself.",
+  metadataBase: new URL("https://echoself-nu.vercel.app"),
   openGraph: {
     title: "EchoSelf",
     description: "See yourself. Hear yourself.",
+    url: "https://echoself-nu.vercel.app",
+    siteName: "EchoSelf",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "EchoSelf logo",
+      },
+    ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "EchoSelf",
     description: "See yourself. Hear yourself.",
+    images: ["/og.png"],
   },
 };
 
